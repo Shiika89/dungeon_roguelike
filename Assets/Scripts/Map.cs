1f915ed538d89a319fd5183a4523ecd531e207bf
shiika89 <kazuki_plant@yahoo.co.jp>
2022-08-27 18:59:28 +0900

    ダンジョンの自動生成のスクリプトを仮導入

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    const int MapWidth = 50;
+    const int MapHeight = 50;
+
+    public int[,] m_map;
+
+    const int wall = 9;
+    const int road = 0;
+
+    public GameObject WallObject;
+
+    void Start()
+    {
+        ResetMapData();
+        CreateDangeon();
+    }
+
+    /// <summary>
+    /// Mapの二次元配列の初期化
+    /// </summary>
+    private void ResetMapData()
+    {
+        m_map = new int[MapHeight, MapWidth];
+        for (int i = 0; i < MapHeight; i++)
+        {
+            for (int j = 0; j < MapWidth; j++)
+            {
+                m_map[i, j] = wall;
+            }
+        }
+    }
+
+    /// <summary>
+    /// マップデータをもとにダンジョンを生成
+    /// </summary>
+    private void CreateDangeon()
+    {
+        for (int i = 0; i < MapHeight; i++)
+        {
+            for (int j = 0; j < MapWidth; j++)
+            {
+                if (m_map[i, j] == wall)
+                {
+                    Instantiate(WallObject, new Vector3(j - MapWidth / 2, i - MapHeight / 2, 0), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
